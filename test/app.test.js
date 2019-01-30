f89367c42e80282a94a57122f3ec7bc849e53590
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-30 10:00:03 -0800

    made color by id test

--- test/app.test.js
+++ test/app.test.js
@@ -46,4 +46,26 @@ describe('colors', () => {
         expect(res.body).toHaveLength(3);
       });
   });
+
+  it('gets a color by id', () => {
+    return createColor('maroon')
+      .then(color => {
+        return Promise.all([
+          Promise.resolve(color._id),
+          request(app)
+            .get(`/color/${color._id}`)
+        ]);
+      })
+      .then(([_id, res]) => {
+        expect(res.body).toEqual({
+          name: 'maroon',
+          hex: '#800000',
+          rgb: 'rgb(128, 0, 0)',
+          __v: 0,
+          _id: 'SOME_ID'
+        });
+      });
+  });
+
+  it('');
 });
