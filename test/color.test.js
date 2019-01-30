72582eb7f02f6330314b864773d9e1e290705d39
Dyanna Tolman <dyannast@gmail.com>
2019-01-30 12:26:20 -0800

    get by id and delete

--- test/color.test.js
+++ test/color.test.js
@@ -5,11 +5,15 @@ const request = require('supertest');
 // const Color = require('../lib/models/Color');
 const app = require('../lib/app');
 
-const createColor = (name, hex, rgb = 'a color') => {
+const createColor = () => {
   return request(app)
-    .then(res => {
-      res.send;
-    });
+    .post('/color')
+    .send({ 
+      name: 'red',
+      hex: '#FF0000',
+      rgb: 'rgb(255, 0, 0)'
+    })
+    .then(res => res.body);
 };
 
 describe('color app', () => {
@@ -54,4 +58,40 @@ describe('color app', () => {
       });
   });
 
+  it('can get a color by id', () => {
+    return createColor('red')
+      .then(createdColor => {
+        return Promise.all([
+          Promise.resolve(createdColor._id),
+          request(app)
+            .get(`/color/${createdColor._id}`)
+        ]);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'red',
+          _id: expect.any(String),
+          __v:0
+        });
+      });
+  });
+
+  it('can delete color by id', () => {
+    return createColor('red')
+      .then(color => {
+        return Promise.all([
+          Promise.resolve(color._id),
+          request(app)
+            .delete(`/color/${color._id}`)
+        ]);
+      })
+      .then(([_id, res]) => {
+        expect(res.body).toEqual({ deleted: 1 });
+        return request(app)
+          .get(`/color/${_id}`);
+      })
+      .then(res => {
+        expect(res.status).toEqual(404);
+      });
+  });
 });
