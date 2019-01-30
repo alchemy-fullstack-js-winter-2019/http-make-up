db9784f1074ec832534909307d7d2b7075f28171
Connor Frendt <cjacksonfx@yahoo.com>
2019-01-30 09:28:58 -0800

    set up basic scaffold?

---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+const app = require('../lib/app');
+const mongoose = require('mongoose');
+const Color = require('../lib/models/Color')
+
+describe('colors', () => {
+  it('can create a new color', () => {
+    
+  })
+})
