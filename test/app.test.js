adf39a76566c96749abf4230d03de34a0717962a
JeiWolfei <jvampy22@gmail.com>
2019-01-30 12:29:18 -0800

    added mongoose schema

--- test/app.test.js
+++ test/app.test.js
@@ -1,108 +1,74 @@
-const request = require('supertest');
+require('dotenv').config();
+require('../lib/utils/connect')();
+const mongoose = require('mongoose');
 const app = require('../lib/app');
-const rimraf = require('rimraf');
-const mkdirp = require('mkdirp');
+const request = require('supertest');
 
-const createcolor = (name, hex, rgb, _id) => {
+const createcolor = ({ name, hex, rgb }) => {
   return request(app)
     .post('/colors')
-    .send({ 
+    .send({
       name: 'red',
       hex: '#FF0000',
       rgb: 'rgb(255, 0, 0)'
-      // eslint-disable-next-line
-      __v: 0,
-      _id: SOME_ID
-     })
+    })
     .then(res => {
       expect(res.body).toEqual({
         name: 'red',
         hex: '#FF0000',
         rgb: 'rgb(255, 0, 0)',
-        _id: expect.any(String),
-        __v: 0
       });
-    });
-  });
-});
-describe('colors', () => {
-  beforeEach(done => {
-    rimraf('./data/colors/*', err => {
-      done(err);
-    });
-  });
-  beforeEach(done => {
-    mkdirp('./data/colors', err => {
-      done(err);
-    });
-  });
-  
-  it('creates a new color', () => {
-    return request(app)
-      .post('/colors')
-      .send({ name: 'red', hex: '#FF0000', rgb:'rgb(255, 0, 0)', _id:  })
-      .then(res => {
-        expect(res.body).toEqual({
-          name: 'red',
-          hex: '#FF0000',
-          rgb: 'rgb(255, 0, 0)',
-          _id: expect.any(String),
-          __v: 0
+    
+    describe('colors', () => {
+      beforeEach(done => {
+        return mongoose.connection.dropDatabase(() => {
+          done();
         });
       });
-  });
-
-  it('gets a color by id', () => {
-    return createcolor('rowboat')
-      .then(createdcolor=> {
-        const _id = createdcolor._id;
+      it('creates a new color', () => {
         return request(app)
-          .get(`/colors/${_id}`)
+          .post('/colors')
+          .send({ name: 'red', hex: '#FF0000', rgb: 'rgb(255, 0, 0)' })
           .then(res => {
             expect(res.body).toEqual({
               name: 'red',
+              hex: '#FF0000',
               rgb: 'rgb(255, 0, 0)',
               _id: expect.any(String),
               __v: 0
             });
           });
+       });
       });
-  });
-  it('updates a color by :id', () => {
-    let newcolor = {
-      name: 'red',
-      hex: ''
-    };
-    return createcolor('red')
-      .then(createdcolor => {
-        const _id = createdcolor._id;
-        return request(app)
-          .put(`/colors/${_id}`)
-          .send(newcolor);
-      })
-      .then(res => {
-        expect(res.body.name).toEqual('hi');
+      it('gets a color by id', () => {
+        return createColor('red')
+          .then(createdColor => {
+            return request(app)
+              .get(`/colors/${_id}`)
+              .then(res => {
+                expect(res.body).toEqual({
+                  name: 'red',
+                  hex: '#FF0000',
+                  rgb: 'rgb(255, 0, 0)',
+                  _id: expect.any(String),
+                  __v: 0
+                });
+              });
+          });
       });
-  });
-  it('can delete a color', () => {
-    return createcolor('gotobed')
-      .then(createdcolor => {
-        const _id = createdcolor._id;
-        return request(app)
-          .delete(`/colors/${_id}`)
+      it('can delete a color', () => {
+        return createColor('red')
+          .then(createdColor => {
+            const _id = createdcolor._id;
+            return request(app)
+              .delete(`/colors/${_id}`);
+          })
           .then(res => {
             expect(res.body).toEqual({ deleted: 1 });
           });
       });
-  });
-  it('error when no color by id', () => {
-    return request(app)
-      .get('/colors/badId')
-      .then(res => {
-        expect(res.status).toEqual(400);
-        expect(res.body).toEqual({ error: 'Bad id: badId' });
+      afterAll(done => {
+        mongoose.connection.close();
+        done();
       });
-  });
 });
-
-
